2026e9d645beb746b3cede4c92c0d759f7b082fc
Joss Manger <jossferatu@gmail.com>
2024-01-24 07:46:18 -0500

    Initial Commit

---
+++ Monitor/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Monitor
+//
+//  Created by Joss Manger on 1/24/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Monitor/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  Monitor
+//
+//  Created by Joss Manger on 1/24/24.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
