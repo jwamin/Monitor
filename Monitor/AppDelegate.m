4519ecdb597c6158c370135c44e8e6922c3c62f5
Joss Manger <jossferatu@gmail.com>
2024-01-27 10:41:58 -0500

    outline status bar menu

--- Monitor/AppDelegate.m
+++ Monitor/AppDelegate.m
@@ -6,18 +6,82 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFAudio/AVFAudio.h>
+
+
+@interface MonitorMonitoring : NSObject
+
+-(void) startMonitoring:(id)sender;
+
+@end
+
+@implementation MonitorMonitoring {
+    id audioThingy;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        audioThingy = [[AVAudioPlayer alloc] init];
+    }
+    return self;
+}
+
+- (void)startMonitoring:(id)sender{
+    
+}
+
+
+@end
+
 
 @interface AppDelegate ()
 
 @property (strong) IBOutlet NSWindow *window;
+@property (strong) NSStatusItem *item;
+
+@property (strong) MonitorMonitoring *model;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    
+    _model = [[MonitorMonitoring alloc] init];
+    
+    
+    NSStatusBar *bar = [NSStatusBar systemStatusBar];
+    _item = [bar statusItemWithLength:NSVariableStatusItemLength];
+    [[_item button] setImage: [NSImage imageWithSystemSymbolName:@"hifispeaker" accessibilityDescription:NULL]];
+    
+    [[_item button] setTitle:@"Monitor"];
+    [[[_item button] cell] setHighlighted:YES];
+    
+    [_item setMenu:[self getMenu]];
+    
 }
 
+-(NSMenu*) getMenu{
+    
+    NSMenu *monitorMenu = [[NSMenu alloc] init];
+    
+    NSMenuItem *isMonitoring = [[NSMenuItem alloc] initWithTitle:@"Is Monitoring" action:NULL keyEquivalent:@""];
+    [isMonitoring setEnabled:TRUE];
+    [isMonitoring setState:NSControlStateValueOn];
+    
+    [monitorMenu insertItem:isMonitoring atIndex:0];
+    
+    NSMenuItem *quit = [[NSMenuItem alloc] initWithTitle:@"Quit" action:NULL keyEquivalent:@"#Q"];
+    [quit setEnabled:YES];
+    
+    [monitorMenu insertItem:quit atIndex:1];
+    
+    return monitorMenu;
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
@@ -30,3 +94,5 @@
 
 
 @end
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+#  Monitor
+
+## Really basic audio passthrough system icon
+
+### Features
+
+1. Enable - enable audio monitoring
+
+2. Disable - disable audio monitoring
+
+3. Quit - Quit the App
+
