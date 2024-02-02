1a236db94d3db2132fe00447cccecbc250554022
Joss Manger <jossferatu@gmail.com>
2024-02-02 08:36:55 -0500

    interface defined by protocol conformance

--- Monitor/MonitoringEngine.h
+++ Monitor/MonitoringEngine.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MonitoringEngine : NSObject
+@protocol JSSYMonitoring <NSObject>
 
 -(void) setup;
 -(void) startMonitoring;
@@ -19,5 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 -(void) terminate;
 
 @end
+
+@interface MonitoringEngine: NSObject<JSSYMonitoring> @end
 
 NS_ASSUME_NONNULL_END
